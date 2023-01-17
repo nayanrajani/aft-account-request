3989d072dc609602bffc9204cb2dbb09d7e1496b
Nayan Rajani <nayanrajani86@gmail.com>
2023-01-17 13:42:09 +0530

    commit

--- account-name.tf
+++ account-name.tf
@@ -29,4 +29,19 @@ module "<name>" {
 
   account_customizations_name = "example-name" // a similar named folder should be created in aft-account-customizations.  
 
+  # provide values - true or false - for all backup schedules (daily, weekly, monthly and yearly)
+  custom_fields = {
+    "daily_backup_enabled"    = true
+    "daily_backup_retention"  = 7
+    "daily_backup_schedule"   = "cron(0 1 * * ? *)"
+    "weekly_backup_enabled"   = false
+    # "weekly_backup_retention" = 5
+    # "weekly_backup_schedule"  = "cron(05 18 ? * SUN *)"
+    "monthly_backup_enabled"   = false
+    # "monthly_backup_retention" = 5
+    # "monthly_backup_schedule"  = "cron(05 18 ? * SUN *)"
+    "yearly_backup_enabled"   = false
+    # "yearly_backup_retention" = 5
+    # "yearly_backup_schedule"  = "cron(05 18 ? * SUN *)"
+  }
 }  
