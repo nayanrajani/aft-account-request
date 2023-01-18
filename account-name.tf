bf3f55e0f9c8580d0ba7b6d121b130865359fb58
Nayan Rajani <nayanrajani86@gmail.com>
2023-01-18 14:54:06 +0530

    coomit

--- account-name.tf
+++ account-name.tf
@@ -32,16 +32,19 @@ module "<name>" {
   # provide values - true or false - for all backup schedules (daily, weekly, monthly and yearly)
   custom_fields = {
     "daily_backup_enabled"    = true
-    "daily_backup_retention"  = 7
-    "daily_backup_schedule"   = "cron(0 1 * * ? *)"
-    "weekly_backup_enabled"   = false
-    # "weekly_backup_retention" = 5
-    # "weekly_backup_schedule"  = "cron(05 18 ? * SUN *)"
-    "monthly_backup_enabled"   = false
-    # "monthly_backup_retention" = 5
-    # "monthly_backup_schedule"  = "cron(05 18 ? * SUN *)"
-    "yearly_backup_enabled"   = false
-    # "yearly_backup_retention" = 5
-    # "yearly_backup_schedule"  = "cron(05 18 ? * SUN *)"
+    "daily_backup_retention"  = 30
+    "daily_backup_schedule"   = "cron(30 19 * * ? *)"
+    #(backup scheduled on every Saturday of the week and retina for 4 weeks)
+    "weekly_backup_enabled"   = true
+    "weekly_backup_retention" = 30
+    "weekly_backup_schedule"  = "cron(30 19 * * SAT *)"
+    #(backup scheduled on every month first week of Sunday and retina for 1 month)
+    "monthly_backup_enabled"   = true
+    "monthly_backup_retention" = 30
+    "monthly_backup_schedule"  = "cron(30 19 ? * 1#1 *)"
+    #(backup scheduled on every year first month & first week of Sunday and retina for 1 year)
+    "yearly_backup_enabled"   = true
+    "yearly_backup_retention" = 365
+    "yearly_backup_schedule"  = "cron(30 19 ? JAN 1#1 *)"
   }
 }  
